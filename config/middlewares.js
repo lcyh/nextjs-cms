5ae3e9782fa069149b4194be1290ab61787b29cd
lcyh <lc173408798@yeah.net>
2022-10-02 18:36:32 +0800

    cms接口 配置跨域限制,只允许 localhost:3000和localhost:1337访问

---
+++ config/middlewares.js
@@ -0,0 +1,20 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    //跨域配置
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["http://localhost:3000", "http://localhost:1337"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
