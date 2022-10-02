8d816cabc62f59451be8cf023c7bdaf1dbbc6140
lcyh <lc173408798@yeah.net>
2022-10-02 18:38:19 +0800

    1.cms接口 过滤非必要的字段 2.接口统一加上 popular=deep,可以拿到深层嵌套的数据

---
+++ src/api/layout/controllers/layout.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { removeTime, removeAttrsAndId } = require("../../../utils");
+
+/**
+ * layout controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::layout.layout", ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: "deep",
+    };
+    const { data } = await super.find(ctx);
+    return removeAttrsAndId(removeTime(data[0]));
+  },
+}));
